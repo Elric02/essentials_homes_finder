6b7f78dc3ce1bccdd816ae4a28ba2947156e2c64
Elric02 <elric.milet@gmail.com>
2025-01-07 00:20:40 +0100

    corrected dir file

--- dir.php
+++ dir.php
@@ -1,5 +1,5 @@
 var filenames = <?php $out = array();
-foreach (glob('file/*.yml') as $filename) {
+foreach (glob('userdata/*.yml') as $filename) {
     $p = pathinfo($filename);
     $out[] = $p['filename'];
 }
