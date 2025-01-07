0c6fe7d848e233a666258c825370348a81cdd0fa
Elric02 <elric.milet@gmail.com>
2025-01-07 01:39:03 +0100

    JSON and async fix

--- homes_search.js
+++ homes_search.js
@@ -6,36 +6,39 @@ document.getElementById('processButton').addEventListener('click', async () => {
 		for (var i = 0; i < data.length; i++) {
 			if (data[i][1] == targetAccountName) {
 				console.log(data[i]);
-				results = JSON.parse(data[i][2]);
+				results = data[i][2];
+				results.replace("'", '%%temp%%');
+				results.replace('"', "'");
+				results.replace("%%temp%%", '"');
+				results = JSON.parse(results);
+				
+				var homes = Object.keys(results);
+				var table = document.getElementById("results").innerHTML;
+				
+				for (var i = 0; i < homes.length; i++) {
+					console.log(results[homes[i]])
+					table += "<tr>"
+					table += "<td>"
+					table += homes[i];
+					table += "</td>"
+					table += "<td>"
+					table += results[homes[i]]["world-name"];
+					table += "</td>"
+					table += "<td>"
+					table += results[homes[i]].x;
+					table += "</td>"
+					table += "<td>"
+					table += results[homes[i]].y;
+					table += "</td>"
+					table += "<td>"
+					table += results[homes[i]].z;
+					table += "</td>"
+					table += "</tr>"
+				}
+				
+				document.getElementById("results").innerHTML = table;
 			}
 		}
 	});
 	
-	console.log(results);
-	
-	var homes = Object.keys(results);
-	var table = document.getElementById("results").innerHTML;
-	
-	for (var i = 0; i < homes.length; i++) {
-		console.log(results[homes[i]])
-		table += "<tr>"
-		table += "<td>"
-		table += homes[i];
-		table += "</td>"
-		table += "<td>"
-		table += results[homes[i]]["world-name"];
-		table += "</td>"
-		table += "<td>"
-		table += results[homes[i]].x;
-		table += "</td>"
-		table += "<td>"
-		table += results[homes[i]].y;
-		table += "</td>"
-		table += "<td>"
-		table += results[homes[i]].z;
-		table += "</td>"
-		table += "</tr>"
-	}
-	
-	document.getElementById("results").innerHTML = table;
 });
