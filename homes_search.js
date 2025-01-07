deb8894461321c68c87dc2c0d02ace88f7bb1130
Elric02 <elric.milet@gmail.com>
2025-01-07 02:03:32 +0100

    Fix splits

--- homes_search.js
+++ homes_search.js
@@ -5,13 +5,8 @@ document.getElementById('processButton').addEventListener('click', async () => {
 	d3.csv("homes.csv", function(data) {
 		for (var i = 0; i < data.length; i++) {
 			if (data[i][1] == targetAccountName) {
-				console.log(data[i]);
+				console.log(data[i][2]);
 				results = data[i][2];
-				//results.replace("'", '%%temp%%');
-				//results.replace('"', "'");
-				//results.replace("%%temp%%", '"');
-				//console.log(results);
-				//results = JSON.parse(results);
 				var homes = results.split('},');
 				
 				var table = document.getElementById("results").innerHTML;
@@ -25,13 +20,13 @@ document.getElementById('processButton').addEventListener('click', async () => {
 					table += homes[i].split("'world-name': '")[1].split("'}")[0];
 					table += "</td>"
 					table += "<td>"
-					table += homes[i].split("'x': '")[1].split(",")[0];
+					table += homes[i].split("'x': ")[1].split(",")[0];
 					table += "</td>"
 					table += "<td>"
-					table += homes[i].split("'y': '")[1].split(",")[0];
+					table += homes[i].split("'y': ")[1].split(",")[0];
 					table += "</td>"
 					table += "<td>"
-					table += homes[i].split("'z': '")[1].split(",")[0];
+					table += homes[i].split("'z': ")[1].split(",")[0];
 					table += "</td>"
 					table += "</tr>"
 				}
